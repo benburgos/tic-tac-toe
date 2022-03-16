95f47840b0fdd243dc27f54546a211554546bd06
Ben Burgos <ben.a.burgos@gmail.com>
2022-03-15 20:28:41 -0700

    adds reset button and styling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 console.log('JS is up and running!')
 
 /*----- Constants -----*/
+// X & O - Players
+// Winning Combinations
 
 /*----- App's State (Variables) -----*/
+// Where the users click (where to put an X or O) - make sure the square is not taken first
+// Whose turn it is
+// When we have a winner or if there is a tie - if game is ongoing
+// Bonus: If the player wants to quit/end the game early
+// Bonus: Score for games won
+// Bonus: Move time limit
 
 /*----- Cached Element References -----*/
+// The Game Squares
+// A 'Reset Game' Button
 
 /*----- Event Listeners -----*/
 
